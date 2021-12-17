12a6bf6acb12693e42188c1208b89272c78912e2
angym7415 <96276596+angym7415@users.noreply.github.com>
2021-12-17 11:48:25 -0600

    Update procesa.js

--- js/procesa.js
+++ js/procesa.js
@@ -7,25 +7,29 @@ function procesa(){
     var Clave = document.getElementById("clave").value,
     Nombre = document.getElementById("nombre").value,
     Telefono = document.getElementById("telefono").value,
+    Personas = document.getElementById("personas").value,
     fecha = document.getElementById("fecha").value;
     
     
     salidaFecha.textContent = "Fecha: "+fecha;
+    salidaPersonas.textContent = "Nuemero de Personas:  "+Personas;
     salidaClave.textContent = "Clave: "+Clave;
     salidaNombre.textContent = "Nombre: "+Nombre;
     salidaTelefono.textContent = "Telefono: "+Telefono;
+    
 
 }
 
 function alerta(){
 
     var Clave = document.getElementById("clave").value,
+    Personas = document.getElementById("personas").value,
     Nombre = document.getElementById("nombre").value,
     Telefono = document.getElementById("telefono").value,
     fecha = document.getElementById("fecha").value;
     
     alert ( "Su reservación se guardo con exito con fecha: "     + fecha + 
-     "Para:  "      + Clave +
+     "Para:  "      + Clave +           "Reservación para:   " +Personas+"personas"
                      "A nombre de:  "        + Nombre + 
                     "Telefono de contacto  " + Telefono);
 
