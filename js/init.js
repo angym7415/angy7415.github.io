1d320d1127d04a5a670e98a1035cf02ea2c6f573
angym7415 <96276596+angym7415@users.noreply.github.com>
2021-12-16 22:02:02 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyBXNMhKyoeh-asedrnFm8Zfxh_hjzl5Ogc",
+  authDomain: "restaurante-6aba8.firebaseapp.com",
+  projectId: "restaurante-6aba8",
+  storageBucket: "restaurante-6aba8.appspot.com",
+  messagingSenderId: "835343829108",
+  appId: "1:835343829108:web:fd3ee88dda1f025da3a921",
+  measurementId: "G-06F2PCC988"
+});
